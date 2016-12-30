26fcd3ad681d7b7eb0392809ebb62ef50103bced
Rocco Howard <rocco@hnh.digital>
2016-12-30 12:23:32 +1100

    Added country population dataset

--- src/Datasets/country_population.php
+++ src/Datasets/country_population.php
@@ -9,5 +9,14 @@
 return [
     'table'   => 'country_population',
     'path'    => 'https://github.com/datasets/population/raw/master/data/population.csv',
-    'mapping' => [],
+    'mapping' => [
+        'Country Name' => 'name',
+        'Country Code' => 'code',
+        'Year'         => 'year',
+        'Value'        => 'population'
+    ],
+    'import_keys' => [
+        'code',
+        'year',
+    ]
 ];
