f4307f9a83df1d6073677a074a6ba9dc2f02d075
Rocco Howard <rocco@hnh.digital>
2016-12-30 12:23:52 +1100

    Added country population dataset, but filtered to the most recent year

--- src/Datasets/country_population_current.php
+++ src/Datasets/country_population_current.php
@@ -7,13 +7,29 @@
  */
 
 return [
-    'table'         => 'country_population_current',
-    'download_path' => 'https://github.com/datasets/population/raw/master/data/population.csv',
-    'mapping'       => [
-        'Year'  => 'year',
-        'Value' => 'total',  
+    'table'   => 'country_population_current',
+    'path'    => 'https://github.com/datasets/population/raw/master/data/population.csv',
+    'mapping' => [
+        'Country Name' => 'name',
+        'Country Code' => 'code',
+        'Year'         => 'year',
+        'Value'        => 'population',  
     ],
-    'filter'        => function($reader) {
-        return $reader->addFilter(function($row) { return $row[2] === date('Y'); });
-    }
+    'filter'  => function($reader) {
+
+        if (!isset($_ENV['population_year'])) {
+            // Find most recent year
+            $year_list = array_unique(iterator_to_array($reader->fetchColumn(2), false));
+            rsort($year_list);
+            $_ENV['population_year'] = $year_list[1];
+        }
+
+        $population_year = $_ENV['population_year'];
+
+        return $reader->addFilter(function($row) use ($population_year) { return array_get($row, 2, 0) === $population_year; });
+    },
+    'import_keys' => [
+        'code',
+        'year',
+    ]
 ];
--- src/Migrations/CreateDataCountryPopulationCurrentTable.php
+++ src/Migrations/CreateDataCountryPopulationCurrentTable.php
@@ -3,11 +3,48 @@
 namespace Bluora\LaravelDatasets\Migrations;
 
 use Bluora\LaravelDatasets\Traits\MigrationsTrait;
-use Illuminate\Support\Facades\Schema;
+use DB;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
 
 class CreateDataCountryPopulationCurrentTable extends Migration
 {
+    use MigrationsTrait;
 
+    protected $table_name = 'data_country_population_current';
+
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create($this->table_name, function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('uuid', 16);
+            $table->string('name', 255)->default('');
+            $table->string('code', 3)->default('');
+            $table->integer('year');
+            $table->bigInteger('population');
+            $table->timestamp('created_at')->default(DB::raw('CURRENT_TIMESTAMP'));
+            $table->timestamp('updated_at')->default(DB::raw('CURRENT_TIMESTAMP on update CURRENT_TIMESTAMP'));
+            $table->timestamp('archived_at')->nullable();
+            $table->timestamp('deleted_at')->nullable();
+        });
+
+        self::updateUuid($this->table_name, 'uuid');
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        self::dropUuidTrigger($this->table_name);
+        self::standardTableDrop($this->table_name);
+    }
 }
