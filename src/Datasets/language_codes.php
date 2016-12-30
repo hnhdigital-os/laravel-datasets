2d62c73cfa65fbecdba9347b358de28e2683dc85
Rocco Howard <rocco@hnh.digital>
2016-12-30 12:05:29 +1100

    Added language code dataset

--- src/Datasets/language_codes.php
+++ src/Datasets/language_codes.php
@@ -10,15 +10,13 @@ return [
     'table'   => 'language_codes',
     'path'    => 'https://github.com/datasets/language-codes/raw/master/data/language-codes-full.csv',
     'mapping' => [
-        'name'                           => 'name',
-        'official_name_en'               => 'official_name_en',
-        'official_name_fr'               => 'official_name_fr',
-        'ISO3166-1-Alpha-2'              => 'iso3166_1_alpha_2',
-        'ISO3166-1-Alpha-3'              => 'iso3166_1_alpha_3',
-        'ISO3166-1-numeric'              => 'iso3166_1_numeric',
-        'Dial'                           => 'dial',
-        'ISO4217-currency_name'          => 'iso4217_currency_name',
-        'ISO4217-currency_minor_unit'    => 'iso4217_currency_minor_unit',
-        'ISO4217-currency_numeric_code'  => 'iso4217_currency_numeric_code',
+        'alpha2'   => 'iso3166_1_alpha_2',
+        'alpha3-b' => 'iso3166_1_alpha_3',
+        'alpha3-t' => 'iso3166_1_alpha_3t',
+        'English'  => 'english',
+        'French'   => 'french',
     ],
+    'import_keys' => [
+        'iso3166_1_alpha_3'
+    ]
 ];
--- src/Migrations/CreateDataLanguageCodesTable.php
+++ src/Migrations/CreateDataLanguageCodesTable.php
@@ -3,11 +3,49 @@
 namespace Bluora\LaravelDatasets\Migrations;
 
 use Bluora\LaravelDatasets\Traits\MigrationsTrait;
-use Illuminate\Support\Facades\Schema;
+use DB;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
 
 class CreateDataLanguageCodesTable extends Migration
 {
+    use MigrationsTrait;
 
+    protected $table_name = 'data_language_codes';
+
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create($this->table_name, function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('uuid', 16);
+            $table->string('iso3166_1_alpha_2', 2)->default('');
+            $table->string('iso3166_1_alpha_3', 3)->default('');
+            $table->string('iso3166_1_alpha_3', 3)->default('');
+            $table->string('english', 25)->default('');
+            $table->string('french', 25)->default('');
+            $table->timestamp('created_at')->default(DB::raw('CURRENT_TIMESTAMP'));
+            $table->timestamp('updated_at')->default(DB::raw('CURRENT_TIMESTAMP on update CURRENT_TIMESTAMP'));
+            $table->timestamp('archived_at')->nullable();
+            $table->timestamp('deleted_at')->nullable();
+        });
+
+        self::updateUuid($this->table_name, 'uuid');
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        self::dropUuidTrigger($this->table_name);
+        Schema::drop($table_name);
+    }
 }
